032cde6fbe8182e0f3df545317583bd799664810
Your Name <liyucheng97@126.com>
2025-12-18 21:26:39 +0100

    Stop tracking build folder

--- build/mpc_car_control/CMakeFiles/mpc_car_control__rosidl_typesupport_c__pyext.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for mpc_car_control__rosidl_typesupport_c__pyext.
--- build/mpc_car_control/CMakeFiles/mpc_car_control__rosidl_typesupport_cpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for mpc_car_control__rosidl_typesupport_cpp.
-# This may be replaced when dependencies are built.
